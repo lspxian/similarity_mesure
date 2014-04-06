584b7615b1dbe97d35286836de58be6359d30381
shuopeng <lishuopeng@gmail.com>
2014-04-06 23:07:39 +0200

    add ti-idf and cosine method

---
+++ simmes/src/fr/shuopeng/tfidf/TfIdfMain.java
@@ -0,0 +1,21 @@
+package fr.shuopeng.tfidf;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class TfIdfMain {
+
+	/**
+     * Main method
+     * @param args
+     * @throws FileNotFoundException
+     * @throws IOException 
+     */
+    public static void main(String args[]) throws FileNotFoundException, IOException
+    {
+        DocumentParser dp = new DocumentParser();
+        dp.parseFiles("testfolder");//location of your source files, only text file
+        dp.tfIdfCalculator(); //calculates tfidf
+        dp.getCosineSimilarity(); //calculated cosine similarity  
+    }
+
+}
